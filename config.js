93dc3ba1ecc9e780eb5da01ac4dc7e70bffa7f01
captainsteve345 <60198275+captainsteve345@users.noreply.github.com>
2020-01-23 11:10:57 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 function randomImg(){
     var randomNumber = Math.floor(Math.random() * 25) + 1;
     var imgName = "img_" + randomNumber + ".jpg";
-    document.getElementById("greenbox").src= "YOUR_IMG_PATH" + "/" + imgName ;
+    document.getElementById("greenbox").src= "IMG Path" + "/" + imgName ;
  }
