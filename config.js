e7fbe70ef744305d9e2357475c992e826d0e3d86
captainsteve345 <60198275+captainsteve345@users.noreply.github.com>
2020-01-23 11:10:36 +0000

    Update and rename site.js to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 function randomImg(){
-    var randomNumber = Math.floor(Math.random() * 12) + 1;
+    var randomNumber = Math.floor(Math.random() * 25) + 1;
     var imgName = "img_" + randomNumber + ".jpg";
     document.getElementById("greenbox").src= "YOUR_IMG_PATH" + "/" + imgName ;
  }
